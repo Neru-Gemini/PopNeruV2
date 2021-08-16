455b55cd7d455e3c142596b2374cde551fdad954
Neru-Gemini <89028986+Neru-Gemini@users.noreply.github.com>
2021-08-16 22:15:25 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,14 @@
+﻿{
+	"version": 1629126813,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/particles.png",
+		"images/particles2.png",
+		"images/particles3.png",
+		"images/particles4.png"
+	]
+}
